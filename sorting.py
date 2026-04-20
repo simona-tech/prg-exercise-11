de8990b64a1094478e6c9f9d10adb43ea6ad0715
Simona Tauberova <257055@vutbr.cz>
2026-04-20 13:26:06 +0200

    Update3

--- sorting.py
+++ sorting.py
@@ -3,17 +3,9 @@ import random
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
 
-values = random_numbers(10)  # 10 čísel v rozsahu 0–100
-small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
-
-def my_selection_sort(number_list):
-    sorted_numbers = sorted(number_list)
-    return sorted_numbers
-
 def selection_sort(values_original):
     values = values_original.copy()
     for min_index in range(len(values)):
-        print(values)
         min_value = values[min_index]
         min_ind = min_index
         for i in range(min_index, len(values)):
@@ -23,9 +15,17 @@ def selection_sort(values_original):
         values[min_ind], values[min_index] = values[min_index], values[min_ind]
     return values
 
+def bubble_sort(values_original):
+    values = values_original.copy()
+    for x in range(len(values)-1):
+        for i in range(len(values)-1-x):
+            if values[i] > values[i+1]:
+                values[i], values[i+1] = values[i+1], values[i]
+    return values
+
 def main():
     val_ori = random_numbers(20)
     return selection_sort(val_ori)
 
 if __name__ == "__main__":
-    print(main())
+    print(bubble_sort(random_numbers(10)))
